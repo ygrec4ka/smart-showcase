3a054e20778f2b8bc5c57e138267a56e744d0888
ygrec4ka <sam06ser@mail.ru>
2026-05-14 21:32:57 +0400

    update product entity: add created_at, updated_at, stock_quantity

--- src/modules/catalog/domain/entities/product.py
+++ src/modules/catalog/domain/entities/product.py
@@ -12,8 +12,10 @@ class Product:
     sku: str
     base_price: Decimal
     company_id: UUID
-    category_id: UUID
     created_at: datetime
+    updated_at: datetime
     id: UUID = field(default_factory=uuid6.uuid7)
+    category_id: Optional[UUID] = None
     description: Optional[str] = None
+    stock_quantity: int = 0
     is_active: bool = True
