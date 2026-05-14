f7a3686edec29120474af3f0c1a752a93c1d6608
ygrec4ka <sam06ser@mail.ru>
2026-05-14 21:32:29 +0400

    update category entity: add slug, created_at, updated_at, is_active attributes

--- src/modules/catalog/domain/entities/category.py
+++ src/modules/catalog/domain/entities/category.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from datetime import datetime
 from uuid import UUID
 import uuid6
 
@@ -6,5 +7,9 @@ import uuid6
 @dataclass
 class Category:
     name: str
+    slug: str
     company_id: UUID
+    created_at: datetime
+    updated_at: datetime
     id: UUID = field(default_factory=uuid6.uuid7)
+    is_active: bool = True
