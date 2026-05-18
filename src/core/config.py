daed78ed9cc33ba2bb90a4657dab411af13e66d2
ygrec4ka <sam06ser@mail.ru>
2026-05-19 00:17:39 +0400

    create new class "AuthJWT": add private_key path, public_key path, algorithm

--- src/core/config.py
+++ src/core/config.py
@@ -40,6 +40,12 @@ class APIPrefix(BaseModel):
     categories: str = "/categories"
 
 
+class AuthJWT(BaseModel):
+    private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
+    algorithm: str = "RS256"
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=(
@@ -55,6 +61,7 @@ class Settings(BaseSettings):
     api: APIPrefix = APIPrefix()
     db: DatabaseConfig
     logging: LoggingConfig = LoggingConfig()
+    auth_jwt: AuthJWT = AuthJWT()
 
 
 settings = Settings()
