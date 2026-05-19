74d6c1706124da0eb0e3a4a106959f94be4aeb52
ygrec4ka <sam06ser@mail.ru>
2026-05-19 15:29:22 +0400

    add expiration to jwt tokens

--- scripts/generate_keys.py
+++ scripts/generate_keys.py
@@ -1,3 +1,5 @@
+from datetime import timedelta, datetime, timezone
+
 import jwt
 from core import settings
 
@@ -6,7 +8,20 @@ def encode_jwt(
     payload: dict,
     private_key: str = settings.auth_jwt.private_key_path.read_text(),
     algorithm: str = settings.auth_jwt.algorithm,
+    expire_minutes: int = settings.auth_jwt.access_token_expire_minutes,
+    expire_timedelta: timedelta | None = None,
 ):
+    to_encode = payload.copy()
+    now = datetime.now(timezone.utc)
+    if expire_timedelta:
+        expire = now + expire_timedelta
+    else:
+        expire = now + timedelta(minutes=expire_minutes)
+    to_encode.update(
+        exp=expire,
+        iat=now,
+    )
+
     encoded = jwt.encode(
         payload,
         private_key,
